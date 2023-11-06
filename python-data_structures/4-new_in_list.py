9fa6956dc1b8f5b39db5fc81b5c74129a2fa16e1
ikalisa1 <ikalisa1@alustudent.com>
2023-11-06 08:05:08 -0800

    4

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -6,7 +6,7 @@ def new_in_list(my_list, idx, element):
         return my_list
     else:
         new_list = []
-    for i in my_list:
-        new_list.append(i)
+        for i in my_list:
+            new_list.append(i)
         new_list[idx] = element
         return new_list
