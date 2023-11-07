2ea273f2f588251112d0ddd77b3dc530a5219c96
ikalisa1 <ikalisa1@alustudent.com>
2023-11-07 05:30:27 -0800

    8

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -3,5 +3,5 @@ def multiple_returns(sentence):
     if len(sentence) == 0:
         tuple_a = (0, None)
     else:
-        tuple_a = (len(sentence), sentence[0]
+        tuple_a = (len(sentence), sentence[0])
     return(tuple_a)
