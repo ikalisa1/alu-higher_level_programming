b4e40448a68e76e896ff4298ff6bf82738681d40
ikalisa1 <ikalisa1@alustudent.com>
2023-11-15 06:02:07 -0800

    2

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -5,20 +5,20 @@
 class Square:
     """Represents a square
     Attributes:
-
         __size (int): size of a side of the square
     """
-    def _init__(self, size=0):
-        """initializes the square
-        Args:
-            size (int): size of a side of the square
-        Returns:
-            none
-        """
-        if type(size) is not int:
-            raise TypeError("size must be an integer")
-        else:
-            if size < 0:       
-                raise ValueError("size must be >= 0")
+    def __init__(self, size=0):
+            """initializes the square
+            Args:
+                size (int): size of a side of the square
+            Returns:
+                None
+            """
+            if type(size) is not int:
+                raise TypeError("size must be an integer")
             else:
-                self.__size = size
+                if size < 0:
+                    raise ValueError("size must be >= 0")
+                else:
+ 
+                    self.__size = size
