fd4b51cec4b81f9860ff97a819c441d38569845a
ikalisa1 <ikalisa1@alustudent.com>
2023-11-15 06:21:38 -0800

    3-square.py

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -22,10 +22,8 @@ class Square:
             else:
                 self.__size = size
     def area(self):
-
         """calculates the square's area
         Returns:
             The area of the square
         """
-
         return (self.__size) ** 2
