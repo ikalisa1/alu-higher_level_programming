1f461f9d883a7c12d6c0494945a67a1ffe99cb46
ikalisa1 <ikalisa1@alustudent.com>
2023-11-15 06:24:47 -0800

    3

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -21,6 +21,7 @@ class Square:
                 raise ValueError("size must be >= 0")
             else:
                 self.__size = size
+
     def area(self):
         """calculates the square's area
         Returns:
