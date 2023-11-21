15ae0a80b18026a9d7ecafdd2dad25847f85b0de
ikalisa1 <ikalisa1@alustudent.com>
2023-11-21 13:00:18 -0800

    1

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -30,7 +30,7 @@ class Rectangle:
         """gets private instance attribute height"""
         return (self.__height)
 
-    @height.setter  
+    @height.setter
     def height(self, value):
         """sets private instance attribute height"""
         if type(value) is not int:
