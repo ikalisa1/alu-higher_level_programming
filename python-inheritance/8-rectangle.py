416937f355cce5c27ab7738d1d43b05430ed079e
ikalisa1 <ikalisa1@alustudent.com>
2023-11-24 05:04:56 -0800

    8

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,19 +1,14 @@
 #!/usr/bin/python3
-'''
-    Implementing a Geometry class
-'''
+"""defines class Rectangle that inherits from BaseGeometry"""
 
 
-BaseGeometry = __import__("7-base_geometry").BaseGeometry
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    '''
-        Implements a rectangle
-    '''
+    """class for rectangle that inherits from BaseGeometry"""
     def __init__(self, width, height):
         self.integer_validator("width", width)
-        self.integer_validator("height", height)
-
         self.__width = width
+        self.integer_validator("height", height)
         self.__height = height
