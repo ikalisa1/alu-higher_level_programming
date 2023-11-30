0bdcd4ec981eb3b9666a6e74483d3b365c13567f
ikalisa1 <ikalisa1@alustudent.com>
2023-11-30 05:46:36 -0800

    4

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-#curl sends POST req to URL, display response body
-curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
-
+# curl sends GET req to URL, displays response body
+curl -sH "X-School-User-Id: 98" "$1"
