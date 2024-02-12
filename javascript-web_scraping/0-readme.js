47d8289529a8f8721f746bffbfe42c58631f06dc
ikalisa1 <ikalisa1@alustudent.com>
2024-02-12 04:13:56 -0800

    0

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,14 +1,15 @@
-t fs = require('fs');
+#!/usr/bin/node
+const fs = require('fs');
 const filePath = process.argv[2];
 if (!filePath) {
-	  console.error('Please provide a file path as an argument');
-	  process.exit(1);
+  console.error('Please provide a file path as an argument');
+  process.exit(1);
 }
 fs.readFile(filePath, 'utf-8', (err, data) => {
-	  if (err) {
-		      console.error(err);
-		    } else {
-			        console.log(data);
-			      }
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(data);
+  }
 }
 );
